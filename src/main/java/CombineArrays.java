0e35fff2dbab8784957a0df3f92cb09dbd2fb5b6
Brian Yang <yangbri1@msu.edu>
2024-12-30 03:06:01 +0000

    Use control flow while loops to concatenate two array of Strings into 1 overarching array of Strings --- had to refresh a little

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -9,6 +9,28 @@ public class CombineArrays {
      */
     public int[] combine(int[] arr1, int[] arr2){
 
-        return null;
+        // initialize an array of Integer with fixed size of arr1.length + arr2.length
+        int[] combinedArray = new int[arr1.length + arr2.length];
+
+        // iterate via arr1 ...
+        int i = 0;
+        while(i < arr1.length){
+            // append each element to empty 'combinedArray'
+            combinedArray[i] = arr1[i];
+            // increment by 1 to continue loop
+            ++i;
+        }
+        // loop via arr2 ...
+        int j = 0;
+        while(j < arr2.length){
+            // append each element of arr2 to combinedArray
+            combinedArray[i] = arr2[j];
+            // still incrementing iterator i so other index in combinedArray would NOT to overriden & continue appending arr2 values to end of arr1
+            ++i;
+            // increment j to continue this nearest loop
+            ++j;
+        }
+
+        return combinedArray;
     }
 }
